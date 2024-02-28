07b6dbdfe6254eda6c4e8eb905746f098477b114
Phan Tien Dung <tiendungphan2k1@gmail.com>
2024-02-28 16:02:01 +0700

    homepage tiktok

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,150 @@
+import classNames from 'classnames/bind';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faEllipsisVertical,
+    faCloudArrowUp,
+    faLanguage,
+    faCoins,
+    faGear,
+    faSignIn,
+    faKeyboard,
+} from '@fortawesome/free-solid-svg-icons';
+import { faCircleQuestion, faUser } from '@fortawesome/free-regular-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+import Image from '~/components/Image';
+import { InboxIcon, UploadIcon } from '~/components/Icons';
+import Search from '../Search';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faLanguage} />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'En',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'Vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
+
+function Header() {
+    const currentUser = true;
+
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
+
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@dung',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get Coins',
+            to: '/feedback',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignIn} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <Link to={config.routes.home} className={cx('logo-link')}>
+                        <img src={images.logo} alt="TikTok" />
+                    </Link>
+                </div>
+                {/* Search */}
+                <Search />
+
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudArrowUp} />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 200]} content="Messages" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <UploadIcon className={cx('upload-icon')} />
+                                </button>
+                            </Tippy>
+                            <div className={cx('inbox')}>
+                                <Tippy delay={[0, 200]} content="Inbox" placement="bottom">
+                                    <button className={cx('action-btn')}>
+                                        <InboxIcon />
+                                    </button>
+                                </Tippy>
+                                <span className={cx('noti-inbox')}>5</span>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <Button text> Upload </Button>
+                            <Button primary> Log in </Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <Image
+                                src="https://uploads.nguoidothi.net.vn/content/6d6e7114-749b-4adf-92e7-b9368eb7e231.jpg"
+                                className={cx('user-avatar')}
+                                alt="Nguyễn Văn A"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+import config from '~/config';
+import * as userService from '~/services/userService';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    const [suggestedUsers, setSuggestedUsers] = useState([]);
+
+    useEffect(() => {
+        userService
+            .getSuggested({ page: 1, perPage: 5 })
+            .then((data) => {
+                setSuggestedUsers(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+            <SuggestedAccounts label="Suggested accounts" data={suggestedUsers} />
+            <SuggestedAccounts label="Following accounts" />
+        </aside>
+    );
+}
+
+export default Sidebar;
